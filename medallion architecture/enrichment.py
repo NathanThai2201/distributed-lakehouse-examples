1427855324421da1bf5fe98c0cbc257c095cdfe6
NathanThai2201 <thainhatanh2201@gmail.com>
2026-03-23 07:46:02 +0700

    up

--- medallion architecture/enrichment.py
+++ medallion architecture/enrichment.py
@@ -9,16 +9,118 @@ spark = SparkSession.builder.appName("demo").getOrCreate()
 
 
 
-df_parquet = spark.read.parquet("./test_folder/yellow_tripdata_2025-01.parquet")
+df_parquet = spark.read.parquet("../test_folder/yellow_tripdata_2025-01.parquet")
 
 #df_parquet = spark.read.parquet("hdfs://instance-20260312-012701:9000/data/yellow_tripdata_2025-01.parquet")
 df_parquet.printSchema()
 df_parquet.limit(15).show()
 
 
+# trip duration:
 df = df_parquet.withColumn(
     "trip_duration_minutes",
     expr("timestampdiff(MINUTE, tpep_pickup_datetime, tpep_dropoff_datetime)")
 )
 
+
+df = df.withColumn("pickup_hour", hour("tpep_pickup_datetime")) \
+       .withColumn("pickup_weekday", dayofweek("tpep_pickup_datetime"))
+
+
+# total fees:
+df = df.withColumn(
+    "total_fees",
+    col("extra") +
+    col("mta_tax") +
+    col("tolls_amount") +
+    col("improvement_surcharge") +
+    col("congestion_surcharge") +
+    col("Airport_fee") +
+    col("cbd_congestion_fee")
+)
+# tip ratio:
+df = df.withColumn(
+    "tip_ratio",
+    when(col("fare_amount") > 0, col("tip_amount") / col("fare_amount"))
+    .otherwise(0)
+)
+
+# cost per data:
+df = df.withColumn(
+    "cost_per_mile",
+    when(col("trip_distance") > 0, col("total_amount") / col("trip_distance")).otherwise(0)
+)
+# speed:
+df = df.withColumn(
+    "avg_speed_mph",
+    when(col("trip_duration_minutes") > 0,
+         col("trip_distance") / (col("trip_duration_minutes") / 60)
+    ).otherwise(0)
+)
+# peak hour flag
+df = df.withColumn(
+    "is_peak_hour",
+    col("pickup_hour").between(7, 9) | col("pickup_hour").between(16, 19)
+)
+# weekend or weekday
+df = df.withColumn(
+    "is_weekend",
+    col("pickup_weekday").isin([1, 7])  # Sunday=1, Saturday=7
+)
+# trip type
+df = df.withColumn(
+    "trip_type",
+    when(col("trip_distance") < 2, "short")
+    .when(col("trip_distance") < 10, "medium")
+    .otherwise("long")
+)
+# is credit card
+df = df.withColumn(
+    "is_card_payment",
+    col("payment_type") == 1  # 1 = credit card in NYC taxi data
+)
+# tipped flag
+df = df.withColumn(
+    "tipped",
+    col("tip_amount") > 0
+)
+# suspicious trips
+df = df.withColumn(
+    "is_suspicious",
+    (col("trip_distance") <= 0) |
+    (col("fare_amount") <= 0) |
+    (col("trip_duration_minutes") <= 0)
+)
+# extreme tip ratio
+df = df.withColumn(
+    "high_tip",
+    col("tip_ratio") > 1  # >100% tip
+)
+# revenue per minute
+df = df.withColumn(
+    "revenue_per_minute",
+    when(col("trip_duration_minutes") > 0,
+         col("total_amount") / col("trip_duration_minutes")
+    ).otherwise(0)
+)
+# passenger efficiency
+df = df.withColumn(
+    "fare_per_passenger",
+    when(col("passenger_count") > 0,
+         col("total_amount") / col("passenger_count")
+    ).otherwise(0)
+)
+
+# rounding data:
+df = df.withColumn("tip_ratio", round(col("tip_ratio"), 2))
+df = df.withColumn("total_fees", round(col("total_fees"), 2))
+df = df.withColumn("cost_per_mile", round(col("cost_per_mile"), 2))
+df = df.withColumn("avg_speed_mph", round(col("avg_speed_mph"), 2))
+df = df.withColumn("fare_per_passenger", round(col("fare_per_passenger"), 2))
+df = df.withColumn("revenue_per_minute", round(col("revenue_per_minute"), 2))
+
+
 df.show()
+
+
+df.write.mode("overwrite").parquet("enriched_nyctripdata")
--- medallion architecture/to_silver.py
+++ medallion architecture/to_silver.py
@@ -8,7 +8,7 @@ spark = SparkSession.builder.appName("demo").getOrCreate()
 
 
 
-df= spark.read.parquet("./test_folder/yellow_tripdata_2025-01.parquet")
+df= spark.read.parquet("../test_folder/yellow_tripdata_2025-01.parquet")
 
 #df_parquet = spark.read.parquet("hdfs://instance-20260312-012701:9000/data/yellow_tripdata_2025-01.parquet")
 #df.printSchema()
@@ -20,12 +20,32 @@ df = df.dropna('all')
 # df.limit(20).show()
 # print("dropped NULLs: ",df.count())
 
-df.dropDuplicates()
+df = df.dropDuplicates()
 
 
-df.limit(20).show()
+
+#df.limit(20).show()
 # print("dropped all duplicates: ",df.count())
 
-#df.fillna('None',subset=['originating_base_num']).show()
+df = df.fillna('None',subset=['originating_base_num']).show()
 
+
+# filtering bad values
+df = df.filter(
+    (col("fare_amount") >= 0) &
+    (col("tip_amount") >= 0) &
+    (col("total_amount") >= 0) &
+    (col("trip_distance") >= 0) &
+    (col("tolls_amount") >= 0) &
+    (col("extra") >= 0) &
+    (col("mta_tax") >= 0) &
+    (col("improvement_surcharge") >= 0)
+)
 #df.sort(col('tolls').asc()).show() 
+
+df = df.withColumn(
+    "trip_duration_minutes",
+    expr("timestampdiff(MINUTE, tpep_pickup_datetime, tpep_dropoff_datetime)")
+)
+
+df = df.filter(col("trip_duration_minutes") > 0)
